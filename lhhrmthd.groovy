9b84643b5da0a4e920630620abbd4110806c47ca
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-05-29 08:38:12 +0530

    Update lhhrmthd.groovy

--- lhhrmthd.groovy
+++ lhhrmthd.groovy
@@ -1,29 +1,28 @@
 def fileLocation = getClass().protectionDomain.codeSource.location
 println fileLocation
 
+
 def desc(name) {
-    description(This is an $name Job DSL job)
-               }
-               
+    description("This is an $name Job DSL job")
+}
+
 def cmd(name) {
     steps {
-      shell(echo Im $name)
-          }
-              }
-              
+        shell("echo I'm $name")
+    }
+}
+
 def pub(name) {
     publishers {
-      downstream('ou_substitution_b_m_athna')
-               }
-               }
-                   
+        downstream('ou_substitution_b_m_athna')
+    }
+}
+
 def axs(name) {                 
     axes {
-      axis {
-        name('corpId')
-        valueString(CC_CLIENTS)
-           }
-         }
-              }
-              
-              
+        axis {
+            name('corpId')
+            valueString(CC_CLIENTS)
+        }
+    }
+}
