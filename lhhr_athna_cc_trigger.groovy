48c09305223ea3f067cee6994b97f51944fd8036
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-05-29 09:15:07 +0530

    Update lhhr_athna_cc_trigger.groovy

--- lhhr_athna_cc_trigger.groovy
+++ lhhr_athna_cc_trigger.groovy
@@ -1,24 +1,14 @@
-package hudson.cli;
 
-import jenkins.model.Jenkins;
-import hudson.model.ListView;
-import groovy.shell.lhhrmthd;
+import  lhhrmthd;
 
-def script = new GroovyShell().parse(new File('lhhrmthd.groovy'))
-Jenkins jenkins = Jenkins.getInstance()
 
-def viewName = 'lhhr_Athna'
-jenkins.addView (new ListView(viewName))
 
 job('Athna_cc_trigger') {
 
     lhhrmthd.desc('Athna_cc_trigger')
     lhhrmthd.cmd('Athna_cc_trigger')
     lhhrmthd.pub('Athna_cc_trigger')
-                   
-    myView = hudson.model.Hudson.instance.getView(viewName)
-    myView.doAddJobToView('athna_cc_trigger') 
-    jenkins.save()
+ 
     
                         }
 
