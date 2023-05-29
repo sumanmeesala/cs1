6617261082e804258c28b8849dc85265ae50c35b
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-05-29 08:40:10 +0530

    Add files via upload

---
+++ lhhr_athna_cc_trigger.groovy
@@ -0,0 +1,23 @@
+package hudson.cli;
+
+import jenkins.model.Jenkins;
+import hudson.model.ListView;
+import lhhrmthd
+
+Jenkins jenkins = Jenkins.getInstance()
+
+def viewName = 'lhhr_Athna'
+jenkins.addView (new ListView(viewName))
+
+job('Athna_cc_trigger') {
+
+    lhhrmthd.desc('Athna_cc_trigger')
+    lhhrmthd.cmd('Athna_cc_trigger')
+    lhhrmthd.pub('Athna_cc_trigger')
+                   
+    myView = hudson.model.Hudson.instance.getView(viewName)
+    myView.doAddJobToView('athna_cc_trigger') 
+    jenkins.save()
+    
+                        }
+
