f7321719455694cce228c48da08a783e2442404b
sumanmeesala <129158238+sumanmeesala@users.noreply.github.com>
2023-09-12 21:16:44 +0530

    Add files via upload

---
+++ first.groovy
@@ -0,0 +1,44 @@
+package hudson.cli;
+
+import hudson.model.*
+import hudson.matrix.*
+import hudson.util.*
+import jenkins.model.Jenkins
+import hudson.model.ListView
+import jenkins.model.GlobalConfiguration
+import javaposse.jobdsl.dsl.DslFactory
+
+
+def environment = env
+def custName = 'NONE'
+
+def jenkins = Jenkins.instance
+def viewName = environment+'_csJobs'
+def existingView = jenkins.getView(viewName)
+
+if (existingView == null) {
+                    def newView = new ListView(viewName)
+                    jenkins.addView(newView)
+                    println("View '$newView' created.")
+                          }
+else {
+   println("View '$viewName' already exists.")
+     }
+
+
+def jobName = environment+'_cs1stjob'
+   
+matrixJob(jobName) {
+
+    description('This is an 1st cc Job DSL job')
+
+
+              
+          println("job ' + $jobName + ' created-2.")
+                  
+
+def myView = jenkins.getView(existingView)
+def job = jenkins.getItem(jobName)
+myView.add(job)
+
+}
---
+++ second.groovy
@@ -0,0 +1,45 @@
+package hudson.cli;
+
+import hudson.model.*
+import hudson.matrix.*
+import hudson.util.*
+import jenkins.model.Jenkins
+import hudson.model.ListView
+import jenkins.model.GlobalConfiguration
+import javaposse.jobdsl.dsl.DslFactory
+
+
+def environment = env
+def custName = 'NONE'
+
+def jenkins = Jenkins.instance
+def viewName = environment+'_csJobs'
+def existingView = jenkins.getView(viewName)
+
+   println("existing View '$existingView' .")
+   
+if (existingView == null) {
+                    def newView = new ListView(viewName)
+                    jenkins.addView(newView)
+                    println("View '$newView' created.")
+                          }
+else {
+   println("View '$viewName' already exists.")
+     }
+
+
+def jobName = environment+'_cs2ndjob'
+   
+matrixJob(jobName) {
+
+    description('This is an 2nd cc Job DSL job')
+
+
+          println("job ' + $jobName + ' created-2.")
+                  
+
+def myView = jenkins.getView(existingView)
+def job = jenkins.getItem(jobName)
+myView.add(job)
+
+}
